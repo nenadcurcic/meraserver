c63e08bbd7bcd9a2763140dd06c486ffbfb8273a
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-08 20:02:13 +0200

    Added Word controller and changed Global.asax

---
+++ Controllers/Api/WordsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MeraServer.Controllers.Api
+{
+    public class WordsController : ApiController
+    {
+    }
+}
