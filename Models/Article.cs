92bcb465e51a92f20de5a6fccd024ecd16505354
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-09 15:06:28 +0200

    Changed Web.config and added Articles model

---
+++ Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MeraServer.Models
+{
+    public class Article
+    {
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+
+    public class ArticlesDbContext : DbContext
+    {
+        public DbSet<Article> TextContainers { get; set; }
+    }
+}
